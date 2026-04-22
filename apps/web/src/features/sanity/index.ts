2016202299c12e6bafb1a6d91c0e68d9c92da1d0
davevanhoorn <1295837+davevanhoorn@users.noreply.github.com>
2026-04-22 15:16:33 +0530

    feat: ✨ add sanity feature barrel export

---
+++ apps/web/src/features/sanity/index.ts
@@ -0,0 +1,6 @@
+export { SANITY_API_VERSION } from './constants/index.js';
+export { SanityContext } from './contexts/sanity-context.js';
+export { SanityContextProvider } from './contexts/sanity-context-provider.js';
+export { createSanityClient } from './providers/sanity-client.js';
+export { BaseRepository } from './repositories/base-repository.js';
+export type { SanityClient, SanityRepository } from './types/index.js';
