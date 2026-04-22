cf7b58fe6209b03601acb0080b643c16e48c1650
davevanhoorn <1295837+davevanhoorn@users.noreply.github.com>
2026-04-22 15:14:29 +0530

    refactor: ♻️ move sanity client to feature providers

---
+++ apps/web/src/features/sanity/providers/sanity-client.ts
@@ -0,0 +1,14 @@
+import { env } from 'next-runtime-env';
+import { createClient } from 'next-sanity';
+
+import { SANITY_API_VERSION } from '../constants/index.js';
+import type { SanityClient } from '../types/index.js';
+
+export function createSanityClient(): SanityClient {
+  return createClient({
+    projectId: env('NEXT_PUBLIC_SANITY_PROJECT_ID'),
+    dataset: env('NEXT_PUBLIC_SANITY_DATASET'),
+    apiVersion: SANITY_API_VERSION,
+    useCdn: true,
+  });
+}
--- apps/web/src/lib/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity';
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2026-04-22',
-  useCdn: true,
-});
