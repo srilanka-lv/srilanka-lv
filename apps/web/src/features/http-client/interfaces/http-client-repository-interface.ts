0d798ca18d37d0d89fa30347fd414cd11eea3a74
davevanhoorn <1295837+davevanhoorn@users.noreply.github.com>
2026-04-23 12:18:19 +0530

    feat: ✨ add http client repository interface

---
+++ apps/web/src/features/http-client/interfaces/http-client-repository-interface.ts
@@ -0,0 +1,6 @@
+export interface HttpClientRepositoryInterface {
+  get<T>(url: string, params?: Record<string, string>): Promise<T>;
+  post<T>(url: string, body?: Record<string, unknown>): Promise<T>;
+  put<T>(url: string, body?: Record<string, unknown>): Promise<T>;
+  delete<T>(url: string): Promise<T>;
+}
